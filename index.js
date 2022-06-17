f5f94f24f2567ec27c83af7ed53bbcb7669c240e
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-06-17 12:32:47 +0300

    commit tree a001885efb1982ed60747acad05b8d9a196e042c

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+
+const textParagraph = document.getElementById('text');
+document.addEventListener('DOMContentLoaded',()=>{
+textParagraph.textContent = 'This is really cool!';
+});
